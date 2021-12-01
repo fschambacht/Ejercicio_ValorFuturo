824a15936727e1d6fac6db43cf6afbd19d63928e
fschambacht <aschambacht@gmail.com>
2021-12-01 00:09:53 -0500

    complete structure of html, complete logic of each values in formula and create functions to calculate and clear, create the file style.css with properties and download file .svg to calculator decorator

--- main.js
+++ main.js
@@ -1,40 +1,71 @@
-let future_value = 110.4;
-let present_value = 100;
-let interest_period = 0.01;
-let total_periods /*= 10*/;
-let option = 4;
+function calculate(){
 
-switch (option){
-  case 1:
-    futureValue();
-    break;
-  case 2:
-    presentValue();
-    break;
-  case 3:
-    interestPeriod();
-    break;
-  case 4:
-    totalPeriods();
-    break;
+  let option = document.getElementById("options").value;
+  let future_value = document.getElementById("future_value").valueAsNumber;
+  let present_value = document.getElementById("present_value").valueAsNumber;
+  let interest_period = (document.getElementById("interest_period").valueAsNumber)/100;
+  let total_periods = document.getElementById("total_periods").valueAsNumber;
+  
+  switch (option){
+    case "1":
+      if (isNaN(present_value) || isNaN(interest_period) || isNaN(total_periods)){
+        alert("Debe ingresar los datos de: valor presente, interes del periodo, total periodos")
+      } else {
+        let result = futureValue(present_value, interest_period, total_periods);
+        document.getElementById("result").innerHTML = result
+      }
+      break;
+    case "2":
+      if (isNaN(future_value) || isNaN(interest_period) || isNaN(total_periods)){
+        alert("Debe ingresar los datos de: valor futuro, interes del periodo, total periodos")
+      } else {
+        let result = presentValue(future_value, interest_period, total_periods);
+        document.getElementById("result").innerHTML = result 
+      }
+      break;
+    case "3":
+       if (isNaN(present_value) || isNaN(future_value) || isNaN(total_periods)){
+        alert("Debe ingresar los datos de: valor futuro, valor presente, total periodos")
+      } else {
+        let result = interestPeriod(future_value, present_value, total_periods);
+        document.getElementById("result").innerHTML = result 
+      }
+      break;
+    case "4":
+       if (isNaN(present_value) || isNaN(future_value) || isNaN(interest_period)){
+        alert("Debe ingresar los datos de: valor futuro, valor presente, interes del periodo")
+      } else {
+        let result = totalPeriods(future_value, present_value, interest_period);
+        document.getElementById("result").innerHTML = result 
+      }
+      break;
+  }
 }
 
-function futureValue(/*present_value, interest_period, total_periods*/){
-  let result = present_value * Math.pow(1+interest_period, total_periods);
-  return console.log(result)
+function futureValue(present_value, interest_period, total_periods){
+  let value = present_value * Math.pow(1+interest_period, total_periods);
+  return value.toFixed(1)
 }
 
-function presentValue(/*future_value, interest_period, total_periods*/){
-  let result = future_value / Math.pow(1+interest_period, total_periods);
-  return console.log(result)
+function presentValue(future_value, interest_period, total_periods){
+  let value = future_value / Math.pow(1+interest_period, total_periods);
+  return value.toFixed(0)
 }
 
-function interestPeriod(){
-  let result =  (Math.pow((future_value/present_value),1/total_periods))-1;
-  return console.log(result)
+function interestPeriod(future_value, present_value, total_periods){
+  let value =  (Math.pow((future_value/present_value),1/total_periods))-1;
+  return value.toFixed(2)
 }
 
-function totalPeriods(){
-  let result = Math.log(future_value/present_value) / Math.log(1+interest_period); 
-  return console.log(result)
+function totalPeriods(future_value, present_value, interest_period){
+  let value = Math.log(future_value/present_value) / Math.log(1+interest_period); 
+  return value.toFixed(0)
 }
+
+function clear_calculator(){
+  document.getElementById("future_value").value = "";
+  document.getElementById("present_value").value = "";
+  document.getElementById("interest_period").value = "";
+  document.getElementById("total_periods").value = "";
+}
+
